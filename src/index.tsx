46f81405a4b49863499b624b5bad62590da1b996
Jihen <tekaya.jihen.91@gmail.com>
2020-03-26 14:14:55 +0100

    redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './App';
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import reducer, { AppState } from './store/reducers/reducer';
+import { createStore } from 'redux'
+
 
 ReactDOM.render(
-    <BrowserRouter>
-       <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
